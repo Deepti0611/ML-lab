1a7c174562b4907e50ffb7bf1c8f0c0ffd4b2808
Deepti0611 <deepukannan2006@gmail.com>
2026-07-15 19:24:00 +0530

    comments added

--- Lab2/A1.py
+++ Lab2/A1.py
@@ -1,26 +1,20 @@
 import pandas as pd
 import numpy as np
 
-def create_matrix():
-    df = pd.read_excel("Lab Session Data.xlsx", sheet_name="Purchase data")
+def create_matrix(): # function to create  matrix 
+    df = pd.read_excel("Lab Session Data.xlsx", sheet_name="Purchase data") #derive data from excel sheet 
     '''
     print(df)
 
-    import pandas as pd
-
-    file = pd.ExcelFile("Lab Session Data.xlsx")
-
-    print(file.sheet_names)'''
-
-
-    X=df[['Candies (#)','Mangoes (Kg)','Milk Packets (#)']].to_numpy()
-    y=df[['Payment (Rs)']].to_numpy()
+    '''
+    X=df[['Candies (#)','Mangoes (Kg)','Milk Packets (#)']].to_numpy() # input feature matrix 
+    y=df[['Payment (Rs)']].to_numpy() # payment feature 
     return X,y
-def rank(X):
+def rank(X): # function to find rank of matrix
     from numpy. linalg import matrix_rank 
     rank=matrix_rank(X)
     return rank
-def cost_of_product(X):
+def cost_of_product(X): # function to find c in Xc=y
     from numpy.linalg import pinv
     a = pinv(X)
     return np.dot(a,y)
