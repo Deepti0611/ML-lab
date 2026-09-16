20176ef3a5e6cd7419196aad970079b1f9ec23c5
Deepti0611 <deepukannan2006@gmail.com>
2026-09-16 20:13:34 +0530

    inital commit

---
+++ Lab7/A1.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+def load_data(): # load data
+    df = pd.read_csv("features.csv") #derive data from csv file
+    return df
+
+def probability(df):
+    count=df['person_id'].value_counts() # count the number of samples for each person
+    prob=count/len(df) # calculate the probability of each person
+    return prob
+def entropy(prob):
+    ent=np.sum(-prob*np.log2(prob)) # calculate the entropy of the probability distribution
+    return ent
+    
+df=load_data() # load the data
+print(df) # print the selected samples
+prob=probability(df) # calculate the probability of each person
+print(prob) # print the probabilities
+ent=entropy(prob) # calculate the entropy of the probability distribution
+print(ent) # print the entropy
